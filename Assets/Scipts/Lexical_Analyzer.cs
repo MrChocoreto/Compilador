21a57c9cded9bb2c6f6955a0baecfb89dc3aacba
ChocoretoStonus <chocoretostonus@gmail.com>
2022-08-28 20:27:49 -0500

    First Try

--- Assets/Scipts/Lexical_Analyzer.cs
+++ Assets/Scipts/Lexical_Analyzer.cs
@@ -1,29 +1,92 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Lexical_Analyzer : MonoBehaviour
 {
     [SerializeField, TextArea] private string Text = default;
-    private int ASCII = default;
+    private int NumLine = default;
 
 
     [ContextMenu("Lexical_Analyzer")]
-    public void Analyzer()
+    void SpaceAnalyzer()
     {
+        // Inicializo el numero de linea siempre en 1
+        NumLine = 1;
+
+        // Creo las variables donde almacenare cada palabra y donde comparare los ASCII
+        int ASCII = default;
+        string word = default;
+
+
+        // Recorro cada letra de la frase para poder identificar cada palabra y saber
+        // el numero de linea de cada linea
         for (int i = 0; i < Text.Length; i++)
         {
+            // Aqui solo le doy el valor ASCII de cada uno de los caracteres
             ASCII = Text[i];
-            if (ASCII >= 65 && ASCII <= 90)
+
+            // Reviso si hay un espacio, un salto de linea o si es el final de
+            // la frase
+            if (ASCII == 32 || ASCII == 10 || i == Text.Length - 1)
             {
-                Debug.Log(Text[i] +" es una Mayuscula");
+
+                // si es cierto entonces es por que ya completamos una palabra
+                // y por si acaso reviso que en el caso de que sea el final de
+                // la frase acomplete la palabra
+                if (i == Text.Length - 1)
+                {
+                    word = word + Text[Text.Length-1];
+                }
+
+
+                // le mando la palabra y el numero de linea al metodo
+                TextAnalyzer(word, NumLine);
+
+                // y vuelvo a vaciar al variable que almacena cada frase
+                word = default;
             }
-            else if (ASCII >= 97 && ASCII <= 122)
+            else
             {
-                Debug.Log(Text[i] + " es una Minuscula");
+                // aqui solo añado una letra cada que recorro los caracteres
+                word = word + Text[i];
+            }
+            if (ASCII == 10)
+            {
+                // y aqui solo voy aumentando la variable cada que hay un salto de linea
+                NumLine++;
             }
         }
     }
 
+    void TextAnalyzer(string Word, int Line)
+    {
+        //int ASCII = default;
+
+        // Recorro cada caracter de la palabra 
+        for (int i = 0; i < Word.Length; i++)
+        {
+            // corroboro que el primer caracter de la palabra sea una letra o un guion bajo
+            if (i==0)
+            {
+                if (Word[i] == 95 || Word[i] >= 65 && Word[i] <= 90 || Word[i] >= 97 && Word[i] <= 122)
+                {
+                    Debug.Log(Word[i] +" es una variable, num. linea "+Line);
+                }
+
+            }
+            else
+            {
+                //if (Word[i] == 95 || Word[i] >= 65 && Word[i] <= 90 || Word[i] >= 97 && Word[i] <= 122)
+                //{
+                //    Debug.Log(Word[i] + " es una variable, num. linea " + Line);
+                //}
+                if (Word[i] == 59)
+                {
+                    Debug.Log(Word[i] + " es un PyC, num. linea " +Line);
+                    break;
+                }
+            }
+
+        }
+    }
 
 }
